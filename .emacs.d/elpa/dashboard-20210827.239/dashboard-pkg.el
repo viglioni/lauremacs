017688343f2961c4d27c8e445ccac32c068ee187
Laura <laura.viglioni@loft.com.br>
2021-09-25 23:45:00 -0300

    feat: added dashboard and edited it to support gifs

---
+++ .emacs.d/elpa/dashboard-20210827.239/dashboard-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dashboard" "20210827.239" "A startup screen extracted from Spacemacs"
+  '((emacs "25.3"))
+  :commit "f7287f026103a44cf290fe737b6b9d841eddcaca" :authors
+  '(("Rakan Al-Hneiti"))
+  :maintainer
+  '("Rakan Al-Hneiti")
+  :keywords
+  '("startup" "screen" "tools" "dashboard")
+  :url "https://github.com/emacs-dashboard/emacs-dashboard")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

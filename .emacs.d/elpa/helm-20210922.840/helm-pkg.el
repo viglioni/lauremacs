6a1100b572c1c1b960817a034ab8337c1693cf05
Laura <laura.viglioni@loft.com.br>
2021-09-26 16:49:55 -0300

    feat: added helm

---
+++ .emacs.d/elpa/helm-20210922.840/helm-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm" "20210922.840" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.8.0"))
+  :commit "06c60b663babdf64383e5dc28b99ac910e04e6c8" :authors
+  '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
+  :maintainer
+  '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
+  :url "https://github.com/emacs-helm/helm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ config/helm-config.el
@@ -0,0 +1,5 @@
+(use-package helm
+  ;:straight t
+  :bind (
+         ("M-x" . 'helm-M-x))
+  )

41dcee83cb18e1e4cfed75aecee88bf677a56c5a
Laura <laura.viglioni@loft.com.br>
2021-09-25 23:43:10 -0300

    feat: added garbage colector configs and defined font

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,1 +1,30 @@
-(load-file (join-path lauremacs-dir "home-page/home-page.el"))
+;;
+;; Garbage colector threshold
+;;
+
+(setq gc-cons-threshold (* 20 1024 1024)) ;; initial threshold
+
+(add-hook 'emacs-startup-hook ;; threshold after init
+          (lambda () (setq gc-cons-threshold (* 128 1024 1024)))) 
+
+
+
+;;
+;; Layout
+;;
+
+(setq inhibit-startup-message t)
+(setq initial-buffer-choice "*lauremacs*")
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)          ; Disable the menu bar
+
+(setq visible-bell t)       ; Set up the visible bell
+
+
+(set-face-attribute 'default nil :font "Source Code Pro" :height 150)
+

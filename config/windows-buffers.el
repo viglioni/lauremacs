08fc1267709f8577a0c63abb491512fcbcd5cae1
Laura <laura.viglioni@loft.com.br>
2021-09-26 23:55:21 -0300

    feat: created functions to split window in one and in two

--- config/windows-buffers.el
+++ config/windows-buffers.el
@@ -5,6 +5,32 @@
 ;;
 
 ;;
+;; funcs 
+;;
+                                        ;TODO: Remove from config file
+
+(defun lauremacs--get-prev-buffers ()
+  "Return list with previous buffers."
+  (seq-filter 'buffer-file-name
+              (delq (current-buffer) (buffer-list))))
+
+(defun lauremacs/window-split-single-column ()
+  "Set the layout to single column."
+  (interactive)
+  (delete-other-windows)
+  (balance-windows))
+
+(defun lauremacs/window-split-double-columns ()
+  "Set the layout to two columns.
+TODO: use perspectives
+TODO: throw if not splitable"
+  (interactive)
+  (let ((prev-buffer (or (car (lauremacs--get-prev-buffers)) (current-buffer))))
+    (delete-other-windows)
+    (set-window-buffer (split-window-right) prev-buffer)
+    (balance-windows)))
+
+;;
 ;; windows-buffers related config
 ;;
 
@@ -17,4 +43,8 @@
          ("C-x <right>" . 'evil-window-right)))
 
 
-
+(general-define-key
+ :prefix "<f19> w"
+ "" '(nil :which-key "windows")
+ "1" '(lauremacs/window-split-single-column :which-key "split single column")
+ "2" '(lauremacs/window-split-double-columns :which-key "split double columns"))

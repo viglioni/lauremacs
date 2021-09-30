da2f0136c2cfead7941abebe994ad7a17f9e8bac
Laura <laura.viglioni@loft.com.br>
2021-09-30 10:23:26 -0300

    feat: added some functions and config of windows/buffers

--- config/windows-buffers.el
+++ config/windows-buffers.el
@@ -39,8 +39,8 @@ TODO: throw if not splitable"
   (interactive)
   (delete-other-windows)
   (let* ((prev-buffers (mapcar 'buffer-name (lauremacs--get-prev-buffers)))
-         (snd-buffer (or (car prev-buffers) "*scratch*"))
-         (trd-buffer (or (nth 1 prev-buffers) "*scratch*"))
+         (snd-buffer (or (car prev-buffers) (current-buffer)))
+         (trd-buffer (or (nth 1 prev-buffers) (current-buffer)))
          (second (split-window-right))
          (third (split-window second nil 'right)))
     (set-window-buffer second snd-buffer)
@@ -48,11 +48,68 @@ TODO: throw if not splitable"
     (balance-windows)))
 
 ;;;###autoload
+(defun lauremacs/window-split-grid ()
+  "Sets the layout to a 2*2 grid"
+  (interactive)
+  (delete-other-windows)
+  (let* ((prev-buffers (mapcar 'buffer-name (lauremacs--get-prev-buffers)))
+         (snd-buffer (or (car prev-buffers) (current-buffer)))
+         (trd-buffer (or (nth 1 prev-buffers) (current-buffer)))
+         (fourth-buffer (or (nth 2 prev-buffers) (current-buffer)))
+         (second (split-window-right))
+         (third (split-window-below))
+         (fourth (split-window second nil 'below)))
+    (set-window-buffer second snd-buffer)
+    (set-window-buffer third trd-buffer)
+    (set-window-buffer fourth fourth-buffer)
+    (balance-windows)))
+
+(defun lauremacs/toggle-current-window-dedication ()
+  "Toggle dedication state of a window."
+  (interactive)
+  (let* ((current-window (selected-window))
+         (dedicated? (window-dedicated-p current-window)))
+    (set-window-dedicated-p current-window (not dedicated?))
+    (message "Window %sdedicated to %s"
+             (if dedicated? "no longer " "")
+             (buffer-name))))
+
+
+;;;###autoload
 (defun lauremacs/switch-to-last-buffer ()
   "Switches to last buffer"
   (interactive)
   (switch-to-buffer (car (lauremacs--get-prev-buffers))))
 
+;;;###autoload
+(defun lauremacs/windw-layout-toggle ()
+  "Toggles between horizontal and vertical layout of two windows.
+TODO: add throwif"
+  (interactive)
+  (when (not (= 2 (count-windows)))
+    (error "Can't toggle window layout when the number of windows isn't two."))
+  (let* ((current-is-horizontal? (car (car (window-tree))))
+	 (first-window (nth 2 (window-tree)))
+	 (second-window (nth 3 (window-tree)))
+	 (split-fn (if current-is-horizontal? #'split-window-horizontally #'split-window-vertically))
+	 )))
+
+;;;###autoload
+(defmacro fp/const-fn-interactive (fn &rest args)
+  "Returns an interactive lambda function that executes fn with given args.
+e.g.
+(fp/const-fn-interactive 'concat \"several \" \"string \" \"args\")"
+  `(lambda () (interactive) (apply ,fn (quote ,args))))
+
+(general-define-key ;; walk through code
+ "s-m" 'left-char
+ "s-," 'next-line
+ "s-." 'previous-line
+ "s-/" 'right-char
+ "C-s-m" 'backward-word
+ "C-s-," 'forward-paragraph
+ "C-s-." 'backward-paragraph
+ "C-s-/" 'forward-word)
 
 ;;
 ;; windows-buffers related config
@@ -64,10 +121,10 @@ TODO: throw if not splitable"
  "<down>" 'evil-window-down
  "<left>" 'evil-window-left
  "<right>" 'evil-window-right
- "k" 'evil-window-up
- "j" 'evil-window-down
- "h" 'evil-window-left
- "l" 'evil-window-right)
+ "m" 'evil-window-left
+ "," 'evil-window-down
+ "." 'evil-window-up
+ "/" 'evil-window-right)
 
 ;;
 ;; Windows binds
@@ -77,7 +134,10 @@ TODO: throw if not splitable"
  "w" '(nil :which-key "windows")
  "w1" '(lauremacs/window-split-single-column :which-key "split single column")
  "w2" '(lauremacs/window-split-double-columns :which-key "split double columns")
- "w3" '(lauremacs/window-split-triple-columns :which-key "split double columns"))
+ "w3" '(lauremacs/window-split-triple-columns :which-key "split double columns")
+ "w4" '(lauremacs/window-split-grid :which-key "split windows in grid")
+ "wt" '(lauremacs/toggle-current-window-dedication :which-key "toggle window dedication")
+ "w=" (list (fp/const-fn-interactive 'balance-windows) :which-key "balance windows"))
 
 
 ;;
@@ -94,3 +154,11 @@ TODO: throw if not splitable"
         :which-key "switch to scratch buffer")
  "bh" '((lambda () (interactive) (switch-to-buffer lauremacs-buffer-name))
         :which-key "switch to home buffer"))
+
+;;
+;; Search binds
+;;
+
+(lauremacs-leader
+  "s" '(nil :which-key "search")
+  "ss" '(helm-swoop :which-key "swoop"))

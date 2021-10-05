53de41a34ca297e1207a98304c8fb52ba14bb2b0
Laura <laura.viglioni@loft.com.br>
2021-10-04 23:18:49 -0300

    feat: added winum

---
+++ .emacs.d/elpa/winum-20190911.1607/winum-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from winum.el  -*- no-byte-compile: t -*-
+(define-package "winum" "20190911.1607" "Navigate windows and frames using numbers." '((cl-lib "0.5") (dash "2.13.0")) :commit "c5455e866e8a5f7eab6a7263e2057aff5f1118b9" :authors '(("Thomas de Beauchêne" . "thomas.de.beauchene@gmail.com")) :maintainer '("Thomas de Beauchêne" . "thomas.de.beauchene@gmail.com") :keywords '("convenience" "frames" "windows" "multi-screen") :url "http://github.com/deb0ch/winum.el")
--- config/windows-buffers.el
+++ config/windows-buffers.el
@@ -156,7 +156,26 @@ e.g.
  "m" 'evil-window-left
  "," 'evil-window-down
  "." 'evil-window-up
- "/" 'evil-window-right)
+ "/" 'evil-window-right
+ "M-0" 'neotree-show)
+
+(defun winum-assign-0-to-neotree ()
+  (when (string-match-p (buffer-name) ".*\\*NeoTree\\*.*") 10))
+
+(use-package winum
+  :init
+  (winum-mode)
+  (add-to-list 'winum-assign-functions #'winum-assign-0-to-neotree)
+  :bind (("M-0" . 'winum-select-window-0-or-10)
+	 ("M-1" . 'winum-select-window-1)
+	 ("M-2" . 'winum-select-window-2)
+	 ("M-3" . 'winum-select-window-3)
+	 ("M-4" . 'winum-select-window-4)
+	 ("M-5" . 'winum-select-window-5)
+	 ("M-6" . 'winum-select-window-6)
+	 ("M-7" . 'winum-select-window-7)
+	 ("M-8" . 'winum-select-window-8)
+	 ("M-9" . 'winum-select-window-9)))
 
 (use-package multiple-cursors
   :bind (:map mc/keymap

c655d9cdcba1987a42c330fe5e09f495b97a7c15
Laura <laura.viglioni@loft.com.br>
2021-10-04 22:16:50 -0300

    feat: added eval binds

--- config/emacs-lisp-mode.el
+++ config/emacs-lisp-mode.el
@@ -1,8 +1,19 @@
+;;
+;; @author Laura Viglioni
+;; 2021
+;; GNU Public License 3.0
+;;
+
+;;
+;; configs related to emacs lisp mode
+;;
 
 
 (lauremacs-major-mode-leader
  :keymaps 'emacs-lisp-mode-map
  "c" '(nil :which-key "compile")
- "cc" 'emacs-lisp-byte-compile
- )
+ "c" '(nil :which-key "eval")
+ "cc" '(emacs-lisp-byte-compile :which-key "byte compile")
+ "eb" '(eval-buffer :which-key "eval-buffer")
+ "er" '(eval-region :which-key "eval region"))
 

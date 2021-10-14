f6b43d49adb5eced63a660e094b00d5c904ec981
Laura <laura.viglioni@loft.com.br>
2021-10-14 10:13:08 -0300

    feat: added peer dependencies

---
+++ .emacs.d/elpa/dap-mode-20211005.2006/dap-mode-pkg.el
@@ -0,0 +1,20 @@
+(define-package "dap-mode" "20211005.2006" "Debug Adapter Protocol mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (lsp-mode "6.0")
+    (bui "1.1.0")
+    (f "0.20.0")
+    (s "1.12.0")
+    (lsp-treemacs "0.1")
+    (posframe "0.7.0")
+    (ht "2.3"))
+  :commit "a225e73026f28b67f1837e80497fe3ec641373f4" :authors
+  '(("Ivan Yonchovski" . "yyoncho@gmail.com"))
+  :maintainer
+  '("Ivan Yonchovski" . "yyoncho@gmail.com")
+  :keywords
+  '("languages" "debug")
+  :url "https://github.com/emacs-lsp/dap-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

720e2d383c1b71f7e695d455046293b59cb98014
Laura <laura.viglioni@loft.com.br>
2021-10-14 10:09:32 -0300

    chore: deleted lsp personal config files and added more mc configs

--- .emacs.d/.mc-lists.el
+++ .emacs.d/.mc-lists.el
@@ -3,12 +3,20 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+	delete-horizontal-space
 	helm-M-x
 	indent-for-tab-command
+	kill-region
+	kmacro-end-macro
+	kmacro-start-macro
+	ns-copy-including-secondary
+	org-self-insert-command
 	paredit-doublequote
+	paredit-forward
 	paredit-forward-barf-sexp
 	paredit-forward-delete
 	paredit-forward-kill-word
+	paredit-forward-slurp-sexp
 	paredit-kill
 	paredit-open-round
 	))

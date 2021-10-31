053b70c66d3ee90c3487ac8dd4d132fbe57b00b9
Laura <laura.viglioni@loft.com.br>
2021-10-31 12:06:48 -0300

    chore: added new keybindings

--- config/haskell.el
+++ config/haskell.el
@@ -7,5 +7,15 @@
 
 (use-package haskell-mode
 	:mode "\\.hs\\'"
-	:hook ((haskell-mode . lsp-deferred)))
+	:hook ((haskell-mode . lsp-deferred))
+	:init
+	(lauremacs-major-mode-leader
+		:keymaps 'haskell-mode-map
+		"s" '(nil :which-key "repl")
+		"ss" '(haskell-interactive-switch :which-key "switch to repl")
+		"sb" '(haskell-process-load-file :which-key "send buffer to repl"))
+	(lauremacs-major-mode-leader
+		:keymaps 'haskell-interactive-mode-map
+		"s" '(nil :which-key "repl")
+		"ss" '(haskell-interactive-switch-back :which-key "switch to code")))
 

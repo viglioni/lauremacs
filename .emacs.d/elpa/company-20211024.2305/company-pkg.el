42977089202edb925dfb8f4ce9fcae6251fe69d9
Laura <laura.viglioni@loft.com.br>
2021-11-10 12:16:59 -0300

    chore: added company

---
+++ .emacs.d/elpa/company-20211024.2305/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20211024.2305" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "811beeade86669e20da35efed5b00de6a5e305ab" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

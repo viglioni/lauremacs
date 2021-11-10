b9e26e1299c9b289b671d23c938083a6dbf1a63e
Laura <laura.viglioni@loft.com.br>
2021-11-10 11:52:49 -0300

    feat: added hindent

--- config/haskell.el
+++ config/haskell.el
@@ -4,6 +4,8 @@
 ;; GNU Public License 3.0
 ;;
 
+(use-package hindent
+	:after haskell-mode)
 
 (use-package haskell-mode
 	:mode "\\.hs\\'"
@@ -13,7 +15,11 @@
 		:keymaps 'haskell-mode-map
 		"s" '(nil :which-key "repl")
 		"ss" '(haskell-interactive-switch :which-key "switch to repl")
-		"sb" '(haskell-process-load-file :which-key "send buffer to repl"))
+		"sb" '(haskell-process-load-file :which-key "send buffer to repl")
+		"=" '(nil :which-key "format")
+		"==" '(hindent-reformat-buffer :which-key "format buffer")
+		"=b" '(hindent-reformat-buffer :which-key "format buffer")
+		"=r" '(hindent-reformat-region :which-key "format region"))
 	(lauremacs-major-mode-leader
 		:keymaps 'haskell-interactive-mode-map
 		"s" '(nil :which-key "repl")

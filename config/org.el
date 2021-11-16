d0e6d2a3fb7938fd309e61db931bed92af58f1f7
Laura <laura.viglioni@loft.com.br>
2021-11-15 21:20:06 -0300

    fix: fixed olivetti config

--- config/org.el
+++ config/org.el
@@ -82,8 +82,7 @@ example: (define-org-cmd :heading 'my-fn :table 'my-fn2)"
 										'lauremacs-org-extensions)
 
 (use-package org
-  :hook '((org-mode . lauremacs/org-mode-setup)
-					(org-mode-hook . olivetti-mode))
+  :hook '((org-mode . lauremacs/org-mode-setup))
 	:custom
 	(org-startup-folded t)
   (org-startup-with-latex-preview nil)
@@ -124,11 +123,12 @@ example: (define-org-cmd :heading 'my-fn :table 'my-fn2)"
 	(org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (use-package olivetti
-	:after org-mode
+	:after org
+	:hook '((org-mode      . olivetti-mode)
+					(olivetti-mode . '(lambda () (set-face-attribute 'olivetti-fringe nil
+																											:background "#d0cec7"))))
 	:custom
 	(olivetti-minimum-body-width 80)
-	(olivetti-style 'fancy)
-	:init
-	(set-face-attribute 'olivetti-fringe nil :background "#d0cec7"))
+	(olivetti-style 'fancy))
 
 

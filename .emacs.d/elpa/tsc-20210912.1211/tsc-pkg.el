e429d1294728780ff91b1f6e200aa7b8e87b70a7
Laura <laura.viglioni@loft.com.br>
2021-11-19 11:34:19 -0300

    chore: peer dependency of ob-typescript

---
+++ .emacs.d/elpa/tsc-20210912.1211/tsc-pkg.el
@@ -0,0 +1,13 @@
+(define-package "tsc" "20210912.1211" "Core Tree-sitter APIs"
+  '((emacs "25.1"))
+  :commit "2acca5c8d2e3dc66d4d0a99831b33140b5a5f973" :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+    ("Jorge Javier Araya Navarro" . "jorgejavieran@yahoo.com.mx"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :keywords
+  '("languages" "tools" "parsers" "dynamic-modules" "tree-sitter")
+  :url "https://github.com/emacs-tree-sitter/elisp-tree-sitter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

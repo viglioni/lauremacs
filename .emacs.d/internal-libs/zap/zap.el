74c3f60569eca1f1793ea9127a7a233a92a0e262
Laura <viglionigr@gmail.com>
2023-01-02 18:17:43 -0300

    feat: added fn to international numbers

--- .emacs.d/internal-libs/zap/zap.el
+++ .emacs.d/internal-libs/zap/zap.el
@@ -1,9 +1,15 @@
-(defun zap-open (number)
+(defun zap-open-br (number)
 	(interactive "sInsert number: ")
 	(fp/upipe number
 		(fp/partial 'replace-regexp-in-string "[^0-9]" "")
 		(fp/partial 'concat "http://wa.me/55")
 		'browse-url))
 
+(defun zap-open (number)
+  (interactive "sInsert number: ")
+	(fp/upipe number
+		(fp/partial 'replace-regexp-in-string "[^0-9]" "")
+		(fp/partial 'concat "http://wa.me/")
+		'browse-url))
 
 (provide 'zap)

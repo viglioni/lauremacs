28193e569b0bd6e5013328cf3b63d7e3a1edd960
Laura <viglionigr@gmail.com>
2023-01-02 18:11:25 -0300

    feat: tab-bar org-roam integration

--- config/tab-bar.el
+++ config/tab-bar.el
@@ -1,6 +1,11 @@
 (require 'projectile)
 (require 'functional)
 
+;;
+;; Re/Naming tabs
+;;
+
+
 (defun lauremacs-tab--crop-name (name)
   "Crop NAME and add ..."
   (let ((len (- lauremacs-tab-width 3)))
@@ -30,12 +35,14 @@
      ((> len lauremacs-tab-width) (lauremacs-tab--crop-name name))
      ((< len lauremacs-tab-width) (lauremacs-tab--fill-with-spaces name)))))
 
-(defun lauremacs-tabs-rename-tab ()
-  "Rename tab with projectile project name."
+(defun lauremacs-tabs-rename-tab (&optional name)
+  "Rename tab with projectile project name.
+If NAME is passed, NAME will be used.
+Else it will be named `Default'."
 	(interactive)
-  (let ((tab-name (if (projectile-project-p)
-					            (projectile-project-name)
-			              "default")))
+  (let ((tab-name (or name
+                      (and (projectile-project-p) (projectile-project-name))
+			                "default")))
     (fp/upipe tab-name
       'lauremacs-tab--format-tab-name
 	    'tab-bar-rename-tab)))
@@ -43,27 +50,31 @@
 (advice-add 'helm-projectile-switch-project
             :after 'lauremacs-tabs-rename-tab)
 
-
 (defun lauremacs-tabs-get-tab-name ()
   "Return current tab name."
   (alist-get 'name (tab-bar--current-tab)))
 
-(defmacro lauremacs-tabs-with-project (&rest body)
-  "Execute BODY when inside a project."
-  `(when (projectile-project-p)
-     ,@body))
+(defun lauremacs-tabs--get-all-tab-names ()
+  (mapcar (fp/partial 'alist-get 'name) (tab-bar-tabs)))
+
+(defun lauremacs-tabs--tab-exists? (name)
+  (let ((tab-name (lauremacs-tab--format-tab-name name))
+        (tabs (lauremacs-tabs--get-all-tab-names)))
+    (contains? tabs tab-name)))
 
 
+;;
+;; Killing tabs / buffers
+;;
 
 (defun lauremacs-tabs--kill-all-buffers (&optional tab-index)
   (interactive)
-  (when tab-index (tab-bar-select-tab tab-index))
+  (when tab-index (tab-bar-select-tab tab-index) (projectile-project-p))
   (mapcar 'kill-buffer (projectile-project-buffers)))
 
 (advice-add 'tab-bar-close-tab :before 'lauremacs-tabs--kill-all-buffers)
 
 (defconst lauremacs-tab-width 17)
-
 
 (defun lauremacs-tab-new-project-tab ()
   "Open a new tab and select a project."
@@ -72,6 +83,23 @@
   (revert-buffer)
   (helm-projectile-switch-project))
 
+
+;;
+;; Opening org roam files in tab
+;;
+
+(defconst lauremacs-tabs-org-roam-tab
+  (lauremacs-tab--format-tab-name "org roam"))
+
+;;;###autoload
+(defun lauremacs-tabs-find-org-roam-node ()
+  "Create/go to org roam tab and find node."
+  (interactive)
+  (unless (lauremacs-tabs--tab-exists? lauremacs-tabs-org-roam-tab)
+    (tab-bar-new-tab)
+    (lauremacs-tabs-rename-tab lauremacs-tabs-org-roam-tab))
+  (tab-bar-select-tab-by-name lauremacs-tabs-org-roam-tab)
+  (org-roam-node-find))
 
 
 (use-package tab-bar

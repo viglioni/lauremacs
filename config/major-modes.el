e83ae2d6d6ebbf7d5e8d515dc56e61c37a65d315
Laura <viglionigr@gmail.com>
2023-12-12 07:38:29 -0300

    feat: avoid magit in new window

--- config/major-modes.el
+++ config/major-modes.el
@@ -89,7 +89,23 @@
     "gs" '(magit-status         :which-key "magit status")
     "gf" '(magit-find-file      :which-key "find file")
     "gd" '(magit-diff-dwim      :which-key "diff")
-    "gp" '(lauremacs-gh-open-pr :which-key "open pr")))
+    "gp" '(lauremacs-gh-open-pr :which-key "open pr"))
+  (setq magit-display-buffer-function
+      (lambda (buffer)
+        (display-buffer
+         buffer
+         (cond ((and (derived-mode-p 'magit-mode)
+                     (eq (with-current-buffer buffer major-mode)
+                         'magit-status-mode))
+                nil)
+               ((memq (with-current-buffer buffer major-mode)
+                      '(magit-process-mode
+                        magit-revision-mode
+                        magit-diff-mode
+                        magit-stash-mode))
+                nil)
+               (t
+                '(display-buffer-same-window)))))))
 
 (with-eval-after-load "magit"
   (defun lauremacs-gh--get-repo ()
@@ -150,7 +166,8 @@
 ;;
 
 (with-eval-after-load "make-mode"
-  (add-hook 'makefile-mode-hook 'whitespace-mode))
+  (add-hook 'makefile-mode-hook 'highlight-indentation-mode)
+  (add-hook 'makefile-mode-hook 'indent-tabs-mode))
 
 ;;
 ;; Reading modes

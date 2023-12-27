dc50613d650df3cb7a32a27179bb8f0075ffc02e
Laura Viglioni <laura@vio.com>
2023-12-27 08:05:10 -0300

    chore: used minor modes to switch keys

--- config/elixir.el
+++ config/elixir.el
@@ -9,18 +9,22 @@
 ;; Elixir
 ;;
 
+(require 'snake-case-mode)
+
 (defun lauremacs-elixir-grep ()
   "Grep in .ex .exs files."
   (interactive)
   (let ((initial-search (read-string "initial query: " (word-at-point))))
     (helm-do-ag (projectile-project-root)
-     nil
-     (concat "-G=*.exs? " initial-search))))
+                nil
+                (concat "-G=*.exs? " initial-search))))
 
 
 (use-package elixir-ts-mode
   :mode "\\.exs?\\'"
   :hook ((elixir-ts-mode . lsp-deferred)
+         (elixir-ts-mode . snake-case-mode)
+         (elixir-ts-mode . switch-semi-colon-mode)
 				 (elixir-ts-mode . (lambda () (add-multiple-into-list 'prettify-symbols-alist
 																											   '((">=" . "≥")
 																												   ("<=" . "≤")
@@ -31,8 +35,7 @@
 																												   ("<-" . "←")
                                                            ("=>" . "⇒")
 																												   ("|>" . "▷")))))
-         (before-save . (lambda () (when (eq major-mode 'elixir-ts-mode)
-                                (lsp-format-buffer)))))
+         (elixir-ts-mode . (lambda () (add-hook 'before-save-hook 'lsp-format-buffer nil t))))
   :init
   ;; download latest release from https://github.com/elixir-lsp/elixir-ls
                                         ;(add-to-list 'exec-path "~/elixir-ls")
@@ -47,38 +50,44 @@
 		"ss"  '(inf-elixir-project			:which-key "go to repl (project)")
 		"=="	'(elixir-format						:which-key "format buffer")))
 
-;; (use-package elixir-mode
-;; 	:mode "\\.ex\\'"
-;; 	:hook ((elixir-mode . lsp-deferred)
-;; 				 (elixir-mode . (lambda () (add-multiple-into-list 'prettify-symbols-alist
-;; 																											'((">=" . "≥")
-;; 																												("<=" . "≤")
-;; 																												("!=" . "≠")
-;; 																												("=~" . "≅")
-;; 																												("<-" . "←")
-;; 																												("->" . "→")
-;; 																												("<-" . "←")
-;;                                                         ("=>" . "⇒")
-;; 																												("|>" . "▷")))))
-;;          (before-save . (lambda ()
-;;                           (when (and (eq major-mode 'elixir-mode)
-;;                                      (boundp 'lsp-mode)
-;;                                      lsp-mode)
-;;                             (lsp-format-buffer)))))
-;; 	:init
-;;   ;; download latest release from https://github.com/elixir-lsp/elixir-ls
-;;   ;(add-to-list 'exec-path "~/elixir-ls")
-;;   (setq lsp-elixir-server-command '("~/lexical/_build/dev/package/lexical/bin/start_lexical.sh"))
-;; 	(lauremacs-major-mode-leader
-;; 		:keymaps 'elixir-mode-map
-;; 		"s"		'(nil											:which-key "repl")
-;; 		"se"	'(inf-elixir-send-line		:which-key "send line")
-;; 		"sb"	'(inf-elixir-send-buffer	:which-key "send buffer")
-;; 		"sr"	'(inf-elixir-send-region	:which-key "send region")
-;; 		"ss"	'(inf-elixir							:which-key "go to repl")
-;; 		"sp"  '(inf-elixir-project			:which-key "go to repl (project)")
-;; 		"=="	'(elixir-format						:which-key "format buffer")))
 
 (use-package inf-elixir
 	:after elixir-ts-mode)
 
+
+(defun lauremacs-elixir-create-file (file-path)
+  "Create file if it does not exist.  FILE-PATH is relative to project root."
+  (let ((full-path (join-path (projectile-project-root) file-path)))
+    (unless (file-exists-p full-path)
+      (write-region "" nil full-path)
+      (message (format "Created file: %s" file-path)))
+    file-path))
+
+(defun lauremacs-elixir-impl-test-file (file)
+  "Return the implementation/test file related to FILE."
+  (cond ((string-match-p "^test" file)
+         (fp/pipe file
+           (fp/replace "^test" "lib")
+           (fp/replace "_test\\.exs$" ".ex")
+           'lauremacs-elixir-create-file
+           ))
+        ((string-match-p "^lib" file)
+         (fp/pipe file
+           (fp/replace "^lib" "test")
+           (fp/replace "\\.ex$" "_test.exs")
+           'lauremacs-elixir-create-file))))
+
+
+(with-eval-after-load "projectile"
+  (projectile-register-project-type
+   'elixir
+   '("mix.exs")
+   :src-dir "lib/"
+   :run ""
+   :compile "mix compile"
+   :install "mix deps.get"
+   :project-file ""
+   :test "mix test"
+   :test-suffix "_test"
+   :related-files-fn '(lauremacs-elixir-impl-test-file)
+   ))

17390b100de620892f03c5325c828afa38ddd26c
Laura Viglioni <laura@vio.com>
2024-07-19 16:12:42 -0300

    feat: added readme

--- .emacs.d/internal-libs/publish-roam/publish-roam.el
+++ .emacs.d/internal-libs/publish-roam/publish-roam.el
@@ -1,3 +1,53 @@
+;;; publish-roam.el --- 
+;; 
+;; Filename: publish-roam.el
+;; Description: 
+;; Author: Laura Viglioni
+;; Maintainer: 
+;; Created: Fri Jul 19 14:44:31 2024 (-0300)
+;; Version: 
+;; Package-Requires: ()
+;; Last-Updated: 
+;;           By: 
+;;     Update #: 0
+;; URL: 
+;; Doc URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
+
+
+
 ;;;;;;;;;;;;;;;;;;;
 ;; Datastructure ;;
 ;;;;;;;;;;;;;;;;;;;
@@ -14,9 +64,10 @@
   "Define the org roam query filtering files by TAG-NAME."
   `(org-roam-db-query
     [:select [nodes:title nodes:file nodes:id]
-             :from tags
-             :inner :join nodes :on (= nodes:id tags:node_id)
-             :where (= tag ,tag-name)]))
+             :from nodes 
+             :inner :join tags :as t1 :on (= nodes:id t1:node_id)
+             :inner :join tags :as t2 :on (= nodes:id t2:node_id)
+             :where (and (= t1:tag ,tag-name) (= t2:tag "publish"))]))
 
 (defun publish-roam--file-timestamp (file-name)
   "Get timestamp from FILE-NAME."
@@ -49,7 +100,7 @@ INFO is a `roam-info' struct."
   (let ((file-path (roam-info-file-path info)))
     (shell-command-to-string
      (format
-      "pandoc -f org -t html5 --css=static/styles.css --standalone %s -o %s/%s.html"
+      "pandoc -f org -t html5 --include-in-header=static/header.html --css=static/styles.css --standalone %s -o %s/%s.html"
       file-path
       publish-dir
       (roam-info-timestamp info)))))
@@ -80,7 +131,7 @@ INFO is a `roam-info' struct."
     <meta charset=\"UTF-8\" />
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
     <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />
-    <title>HTML 5 Boilerplate</title>
+    <title>%s</title>
     <link rel=\"stylesheet\" href=\"static/styles.css\" />
     <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\" />
     <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin />
@@ -102,6 +153,7 @@ INFO is a `roam-info' struct."
 </html>
 "
    title
+   title
    (string-join (mapcar 'publish-roam--schema-card info-list) " ")))
 
 (defun publish-roam--index-html (info-list publish-dir header-title)
@@ -111,16 +163,39 @@ INFO is a `roam-info' struct."
    nil
    (format "%s/index.html" publish-dir)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Fix roam links in HTML files ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun publish-roam--fix-links (info-list publish-dir)
+  "Fix links in HTML files inside PUBLISH-DIR using INFO-LIST."
+  (let ((files (directory-files publish-dir t "\\.html$")))
+    (cl-loop
+     for file in files do
+     (cl-loop
+      for info in info-list do
+      (print (shell-command-to-string
+              (format "gsed -i 's/id:%s/%s.html/g' %s"
+                      (roam-info-id info)
+                      (roam-info-timestamp info)
+                      file)))))))
+
 ;;;;;;;;;;;;;;;;
 ;; Public API ;;
 ;;;;;;;;;;;;;;;;
 
 (defun publish-roam-by-tag (tag publish-dir header-title)
-  "Convert all org-roam files with TAG to html inside PUBLISH-DIR.
+  "Publish all org-roam files with tag TAG and tag \"publish\".
+The files will be converted to html inside PUBLISH-DIR.
 An index page will be generated with links to each page using also HEADER-TITLE."
   (let ((info-list (publish-roam--files-by-tag tag)))
     (publish-roam--clean publish-dir)
     (publish-roam--publish-all info-list publish-dir)
     (publish-roam--index-html info-list publish-dir header-title)
-    (print (publish-roam--schema-index header-title info-list))))
+    (publish-roam--fix-links info-list publish-dir)))
 
+
+(provide 'publish-roam)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; publish-roam.el ends here
